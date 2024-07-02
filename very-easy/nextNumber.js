cf20f3c0bf905b9710a8f771f3fe18c807f005a7
Dan Carlton <danlouis.carlton@gmail.com>
2024-07-02 14:38:39 -0700

    challenge complete

---
+++ very-easy/nextNumber.js
@@ -0,0 +1,21 @@
+// Return the Next Number from the Integer Passed
+// Create a function that takes a number as an argument, increments the number by +1 and returns the result.
+
+// Examples
+// addition(0) ➞ 1
+
+// addition(9) ➞ 10
+
+// addition(-3) ➞ -2
+// Notes
+// Don't forget to return the result.
+// If you get stuck on a challenge, find help in the Resources tab.
+// If you're really stuck, unlock solutions in the Solutions tab.
+
+function nextNumber(num) {
+  return num + 1
+}
+
+// console.log(nextNumber(0))
+// console.log(nextNumber(9))
+// console.log(nextNumber(-3))
